189f57a8d7fab2e8f9818a98d861e24ddcb18da9
Elena Nefedova <elenanefedova@gmail.com>
2022-09-20 00:24:08 +0300

    Added createdBy, replyOwnerId, replyPostId, comments

---
+++ src/main/kotlin/posts/Comments.kt
@@ -0,0 +1,9 @@
+package posts
+
+data class Comments(
+    val count: Int = 0,
+    val canPost: Boolean = true,
+    val groupsCanPost: Boolean = false,
+    val canClose: Boolean = false,
+    val canOpen: Boolean = false
+)
